da53f64e08c326468c90885ad85ba859981975ef
LeeCherry731 <sangevol0@gmail.com>
2022-09-04 10:28:01 +0700

    add factory and model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Customer;
+use App\Models\Product;
+use App\Models\Stock;
+use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +27,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::factory(10)->create();
+        Customer::factory(10)->create();
+        Category::factory(10)->create();
+        Stock::factory(10)->create();
+        User::factory(10)->create();
+        Supplier::factory(10)->create();
     }
 }
